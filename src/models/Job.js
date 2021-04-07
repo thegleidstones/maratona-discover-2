633fdd86e8a2986a21e954f650629bff3e181b8a
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-04-07 01:39:55 -0300

    refactor to use database on model Job

--- src/models/Job.js
+++ src/models/Job.js
@@ -1,34 +1,55 @@
-let data = [
-    {
-        id: 1,
-        name: "Pizzaria Guloso",
-        "daily-hours": 4,
-        "total-hours": 1,
-        created_at: Date.now()
-    },
-    {
-        id: 2,
-        name: "Project OneTwo",
-        "daily-hours": 3,
-        "total-hours": 75,
-        created_at: Date.now()
-    },
-]
+const Database = require('../db/config')
 
 module.exports = {
-    create(newJob) {
-        data.push(newJob)
+    async create(job) {
+        const db = await Database()
+
+        await db.run(`
+            INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                created_at
+            ) VALUES (
+                "${job.name}",
+                ${job.daily_hours},
+                ${job.total_hours},
+                ${job.created_at}
+            );
+        `)
+
+        await db.close()
     },
 
-    delete(id) {
-        data = data.filter(job => Number(job.id) !== Number(id))
+    async delete(id) {
+        const db = await Database()
+
+        await db.run(`DELETE FROM jobs WHERE id = ${id}`)
+
+        await db.close()
     },
 
-    get() {
+    async get() {
+        const db = await Database()
+
+        const data = await db.all(`SELECT * FROM jobs`)
+
+        await db.close()
+
         return data
     },
 
-    update(newData) {
-        data = newData
+    async update(job) {
+        const db = await Database()
+
+        await db.run(`
+            UPDATE jobs SET
+            name = "${job.name}",
+            daily_hours = ${job.daily_hours},
+            total_hours = ${job.total_hours}
+            WHERE id = ${job.id}
+        `)
+
+        await db.close()
     }
 }
