b663f92f3cc500ab4c72cb7fd0194770162dc930
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-04-10 23:20:36 -0300

    feat: create categories routes

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,19 @@ const routes = express.Router()
 const ProfileController = require('./controllers/ProfileController')
 const JobController = require('./controllers/JobController')
 const DashboardController = require('./controllers/DashboardController')
+const CategoryController = require('./controllers/CategoryController')
 
 routes.get('/', DashboardController.index)
+
+routes.get('/category', CategoryController.create)
+routes.post('/category', CategoryController.save)
+
 routes.get('/job', JobController.create)
 routes.post('/job', JobController.save)
 routes.get('/job/:id', JobController.edit)
 routes.post('/job/:id', JobController.update)
 routes.post('/job/delete/:id', JobController.delete)
+
 routes.get('/profile', ProfileController.index)
 routes.post('/profile', ProfileController.update)
 
