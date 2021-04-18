e8e3f89828879a34a0ef2cae08bf951c80fc7bfb
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-04-17 23:46:06 -0300

    create middleware to verify if user is logged or not

---
+++ src/middlewares/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+    isAuthenticated(req, res, next) {
+        if (req.user) {
+            next()
+        } else {
+            res.status(401).send('Not Logged In')
+        }
+    }
+}
