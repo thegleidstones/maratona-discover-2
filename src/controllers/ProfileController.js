c9d71e5d0134f4cafe303a25ae21523984fa4146
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-04-17 23:50:26 -0300

    create method to save new profiles and username to get profiles

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,14 +1,33 @@
+const { create } = require('../models/Job')
 const Profile = require('../models/Profile')
 const JobUtils = require('../utils/JobUtils')
 
 module.exports = {
     async index(req, res) {
-        return res.render('profile', { profile: await Profile.get() })
+        const { username } = req.user
+        return res.render('profile', { profile: await Profile.get(username) })
+    },
+
+    async save(user) {
+        const { username, profileUrl, displayName } = user
+        const avatar = user.photos[0].value
+
+        const github_profile = {
+            user_name: username,
+            profile_url: profileUrl,
+            name: displayName,
+            avatar: avatar,
+            created_at: Date.now(),
+            updated_at: Date.now()
+        }
+
+        await Profile.create(github_profile)
     },
 
     async update(req, res) {
+        const { username } = req.user
         const valueHour = JobUtils.valueHour(req)
-        const profile = await Profile.get()
+        const profile = await Profile.get(username)
 
         await Profile.update({
             ...profile,
