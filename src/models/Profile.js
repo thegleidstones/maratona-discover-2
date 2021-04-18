c4cd52284573203daceffaf0c71b06202f081e2c
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-04-17 23:49:10 -0300

    create new method to save new profiles, and modify update and get methods add a WHERE clause

--- src/models/Profile.js
+++ src/models/Profile.js
@@ -1,10 +1,34 @@
 const Database = require('../db/config')
 
 module.exports = {
-    async get() {
+    async create(profile) {
         const db = await Database()
 
-        const data = await db.get(`SELECT * FROM profile`)
+        await db.run(
+            `INSERT INTO profile (
+                user_name,
+                profile_url,
+                name,
+                avatar,
+                value_hour,
+                created_at,
+                updated_at
+            ) VALUES (
+                "${profile.user_name}",
+                "${profile.profile_url}",
+                "${profile.name}",
+                "${profile.avatar}",
+                0,
+                ${profile.created_at},
+                ${profile.updated_at}
+            );`
+        )
+    },
+
+    async get(user_name) {
+        const db = await Database()
+
+        const data = await db.get(`SELECT * FROM profile WHERE user_name = "${user_name}"`)
 
         await db.close()
 
@@ -16,14 +40,13 @@ module.exports = {
 
         db.run(
             `UPDATE profile SET 
-                name = "${profile.name}",
-                avatar = "${profile.avatar}",
                 monthly_budget = ${profile.monthly_budget},
                 hours_per_day = ${profile.hours_per_day},
                 days_per_week = ${profile.days_per_week},
                 vacation_per_year = ${profile.vacation_per_year},
                 value_hour = ${profile.value_hour},
-                updated_at = ${profile.updated_at}`
+                updated_at = ${profile.updated_at}
+            WHERE user_name = "${profile.user_name}"`
         )
 
         await db.close()
